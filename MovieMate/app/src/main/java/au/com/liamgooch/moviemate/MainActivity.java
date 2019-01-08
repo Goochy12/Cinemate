37b176b5ca31c5e41a491a7364b8347ef8d0055b
Liam <lgoo0003@student.monash.edu>
2019-01-09 00:54:36 +1100

    attempted to add nav drawer

--- MovieMate/app/src/main/java/au/com/liamgooch/moviemate/MainActivity.java
+++ MovieMate/app/src/main/java/au/com/liamgooch/moviemate/MainActivity.java
@@ -1,13 +1,47 @@
 package au.com.liamgooch.moviemate;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout navDrawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+    private ActionBar actionbar;
+    private ActionBarDrawerToggle toggle;
+
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Objects.requireNonNull(getSupportActionBar()).setTitle("");
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        actionbar = getSupportActionBar();
+//
+//        navDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        navDrawerLayout.addDrawerListener(toggle);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setHomeButtonEnabled(true);
+
     }
 }
