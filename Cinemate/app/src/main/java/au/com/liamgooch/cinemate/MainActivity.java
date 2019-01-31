3e1827dcaa24b0556848234455c13f96357df37d
Liam <liamgooch@live.com.au>
2019-01-31 15:46:11 +1100

    updated app with files

--- Cinemate/app/src/main/java/au/com/liamgooch/cinemate/MainActivity.java
+++ Cinemate/app/src/main/java/au/com/liamgooch/cinemate/MainActivity.java
@@ -1,14 +1,155 @@
 package au.com.liamgooch.cinemate;
 
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import au.com.liamgooch.cinemate.adapters.HomePageRecyclerAdapter;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.material.navigation.NavigationView;
 
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout navDrawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+    private ActionBar actionbar;
+    private ActionBarDrawerToggle toggle;
+
+    private RecyclerView recyclerView;
+    private LinearLayoutManager recyclerViewLayoutManager;
+    private HomePageRecyclerAdapter homePageRecyclerAdapter;
+
+    private ArrayList<ArrayList<String>> movieList = new ArrayList<>();
+    private MovieDataGetter movieDataGetter;
+
+    private ProgressBar mainProgressBar;
+    private AdView mAdView;
+
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+//        setTheme(R.style.SplashStyle);
+
         super.onCreate(savedInstanceState);
+        setTheme(R.style.CinemateMainTheme);
         setContentView(R.layout.activity_main);
+
+        mainProgressBar = (ProgressBar) findViewById(R.id.mainProgressBar);
+        mainProgressBar.setVisibility(View.VISIBLE);
+        mainProgressBar.animate();
+
+        MobileAds.initialize(this, String.valueOf(R.string.add_mob_app_id));
+
+//        Objects.requireNonNull(getSupportActionBar()).setTitle("");
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+
+        navDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(
+                this, navDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        //drawer.setDrawerListener(toggle);
+        navDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        //recycler view
+        recyclerView = (RecyclerView) findViewById(R.id.new_releases_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        recyclerViewLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(recyclerViewLayoutManager);
+
+        // specify an adapter (see also next example)
+        homePageRecyclerAdapter = new HomePageRecyclerAdapter(movieList,this);
+        recyclerView.setAdapter(homePageRecyclerAdapter);
+
+        movieDataGetter = new MovieDataGetter(this);
+        movieDataGetter.getReleaseCardData();
+
+//        mAdView = findViewById(R.id.adView);
+//        AdRequest adRequest = new AdRequest.Builder().build();
+//        mAdView.loadAd(adRequest);
+}
+
+    public void updateNewReleaseCards(ArrayList<ArrayList<String>> new_releases){
+        mainProgressBar.setVisibility(View.GONE);
+        homePageRecyclerAdapter.setMovieList(new_releases);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.appbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.nav_account) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.nav_about:
+                break;
+            case R.id.nav_settings:
+                break;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
---
+++ Cinemate/app/src/main/java/au/com/liamgooch/cinemate/MovieItem.java
@@ -0,0 +1,238 @@
+package au.com.liamgooch.cinemate;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+
+import static au.com.liamgooch.cinemate.String_Values.ACTORS_CARD;
+import static au.com.liamgooch.cinemate.String_Values.NOTITLE_DETAILS_CARD;
+import static au.com.liamgooch.cinemate.String_Values.TITLE_BULLET_CARD;
+import static au.com.liamgooch.cinemate.String_Values.TITLE_DETAILS_CARD;
+import static au.com.liamgooch.cinemate.String_Values.TAG;
+
+public class MovieItem {
+    private ArrayList<String> importantInfo;
+    private String location;
+    private String movie_id;
+    private String title;
+    private String synopsis;
+    private String runtime;
+    private String genre;
+    private String rating;
+    private String release;
+    private String poster_link;
+
+    private ArrayList<ActorItem> actorList = new ArrayList<>();
+    private ArrayList<ArrayList<String>> key_storylines = new ArrayList<>();
+    private ArrayList<String> key_information = new ArrayList<>();
+    private ArrayList<String> other_information = new ArrayList<>();
+
+    private ArrayList<Integer> cardList = new ArrayList<>();
+
+    private int size;
+
+    public MovieItem(){
+        this.size = 0;;
+    }
+
+    public MovieItem(ArrayList<String> importantInfo, ArrayList<ActorItem> actorList, ArrayList<ArrayList<String>> key_storylines,
+                     ArrayList<String> key_information, ArrayList<String> other_information){
+        this.importantInfo = importantInfo;
+        location = importantInfo.get(0);
+        movie_id = importantInfo.get(1);
+        title = importantInfo.get(2);
+        synopsis = importantInfo.get(3);
+        genre = importantInfo.get(4);
+        runtime = importantInfo.get(5);
+        poster_link = importantInfo.get(6);
+        //release
+        //rating
+
+        this.actorList = actorList;
+        this.key_storylines = key_storylines;
+        this.key_information = key_information;
+        this.other_information = other_information;
+
+        setSize(sizeSetter());
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    private int sizeSetter(){
+        int s = 0;
+
+        if (importantInfo != null){
+            cardList.add(TITLE_DETAILS_CARD);
+            s += 1;
+        }
+        if (synopsis != null){
+            cardList.add(NOTITLE_DETAILS_CARD);
+            s += 1;
+        }
+        if (actorList != null){
+            cardList.add(ACTORS_CARD);
+        }
+        if (key_storylines != null){
+            for (int i = 0; i < key_storylines.size(); i++){
+                cardList.add(TITLE_BULLET_CARD);
+                s += 1;
+            }
+        }
+        if (key_information != null){
+            for (int i = 0; i < key_information.size(); i++){
+                cardList.add(TITLE_BULLET_CARD);
+                s += 1;
+            }
+        }
+        if (other_information != null){
+            for (int i = 0; i < other_information.size(); i++){
+                cardList.add(TITLE_BULLET_CARD);
+                s += 1;
+            }
+        }
+
+        return s;
+    }
+
+    public ArrayList<String> getImportantInfo() {
+        return importantInfo;
+    }
+
+    public void setImportantInfo(ArrayList<String> importantInfo) {
+        this.importantInfo = importantInfo;
+    }
+
+    public ArrayList<ActorItem> getActorList() {
+        return actorList;
+    }
+
+    public void setActorList(ArrayList<ActorItem> actorList) {
+        this.actorList = actorList;
+    }
+
+    public ArrayList<ArrayList<String>> getKey_storylines() {
+        return key_storylines;
+    }
+
+    public void setKey_storylines(ArrayList<ArrayList<String>> key_storylines) {
+        this.key_storylines = key_storylines;
+    }
+
+    public ArrayList<String> getKey_information() {
+        return key_information;
+    }
+
+    public void setKey_information(ArrayList<String> key_information) {
+        this.key_information = key_information;
+    }
+
+    public ArrayList<String> getOther_information() {
+        return other_information;
+    }
+
+    public void setOther_information(ArrayList<String> other_information) {
+        this.other_information = other_information;
+    }
+
+    public int getKeyStorylinesSize(){
+        if (key_storylines != null){
+            return key_storylines.size();
+        }else {
+            return 0;
+        }
+    }
+
+    public int getKeyInfoSize(){
+        if (key_information != null){
+            return key_information.size();
+        }else {
+            return 0;
+        }
+    }
+
+    public ArrayList<Integer> getCardList() {
+        return cardList;
+    }
+
+    public void setCardList(ArrayList<Integer> cardList) {
+        this.cardList = cardList;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getMovie_id() {
+        return movie_id;
+    }
+
+    public void setMovie_id(String movie_id) {
+        this.movie_id = movie_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public String getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(String runtime) {
+        this.runtime = runtime;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getPoster_link() {
+        return poster_link;
+    }
+
+    public void setPoster_link(String poster_link) {
+        this.poster_link = poster_link;
+    }
+
+    public String getRelease() {
+        return release;
+    }
+
+    public void setRelease(String release) {
+        this.release = release;
+    }
+}
