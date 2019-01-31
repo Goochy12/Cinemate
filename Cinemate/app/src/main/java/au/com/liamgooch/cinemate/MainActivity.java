3c7ec1b61af73a32f53f254b552be9e995511150
Liam <liamgooch@live.com.au>
2019-01-31 14:37:23 +1100

    added new project

---
+++ Cinemate/app/src/main/java/au/com/liamgooch/cinemate/MainActivity.java
@@ -0,0 +1,13 @@
+package au.com.liamgooch.cinemate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
