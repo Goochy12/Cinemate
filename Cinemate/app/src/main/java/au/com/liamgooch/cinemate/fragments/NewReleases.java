24160fcd900ad275585e4fa7cd6fdcc09f55a90a
Liam <lgoo0003@student.monash.edu>
2019-02-04 20:58:52 +1100

    created new releases fragment

---
+++ Cinemate/app/src/main/java/au/com/liamgooch/cinemate/fragments/NewReleases.java
@@ -0,0 +1,32 @@
+package au.com.liamgooch.cinemate.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import au.com.liamgooch.cinemate.R;
+
+public class NewReleases extends Fragment {
+
+    public NewReleases() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_releases, container, false);
+    }
+
+}
