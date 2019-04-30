c3cf3a95f32282aad9f546d30d9368d084bd347f
Liam <lgoo0003@student.monash.edu>
2019-04-30 13:28:50 +1000

    updated email sending

--- db_entry/summary.php
+++ db_entry/summary.php
@@ -5,77 +5,62 @@
 </head>
 
 <body>
-	<?php 
-	
+	<?php
+
 	//important info
-	$title = $_POST["title"];
-	$release = $_POST["release"];
-	$rating = $_POST["rating"];
-	$runtime = $_POST["runtime"];
-	$genre = $_POST["genre"];
-	$trailer = $_POST["trailer"];
-	$poster_link = $_POST["poster_link"];
-	$synopsis = $_POST["genre"];
-	
+	$title = $_POST[ "title" ];
+	$release = $_POST[ "release" ];
+	$rating = $_POST[ "rating" ];
+	$runtime = $_POST[ "runtime" ];
+	$genre = $_POST[ "genre" ];
+	$trailer = $_POST[ "trailer" ];
+	$poster_link = $_POST[ "poster_link" ];
+	$synopsis = $_POST[ "genre" ];
+
+	$dbEntry = strtolower( $title );
+	$dbEntry = str_replace( " ", "_", $dbEntry );
+	//add release date
+
 	//storylines
 	//movie
 	$movieSLArray = array();
-	foreach($_POST["movieStoryLine"] as $value){
-		array_push($movieSLArray,$value);
+	foreach ( $_POST[ "movieStoryLine" ] as $value ) {
+		array_push( $movieSLArray, $value );
 	}
-	$movieStoryLines = implode("<br>",$movieSLArray);
-	
+	$movieStoryLines = implode( "<br><li>", $movieSLArray );
+
 	//real
 	$realSLArray = array();
-	foreach($_POST["realStoryLine"] as $value){
-		array_push($realSLArray,$value);
+	foreach ( $_POST[ "realStoryLine" ] as $value ) {
+		array_push( $realSLArray, $value );
 	}
-	$realStoryLines = implode("<br>",$realSLArray);
+	$realStoryLines = implode( "<br><li>", $realSLArray );
 
 	//Actors
 	$actorArray = array();
 	//name
-	foreach($_POST["actorName"] as $value){
-		array_push($actorArray,array($value));
+	foreach ( $_POST[ "actorName" ] as $index => $value ) {
+		array_push( $actorArray, array( $value ) );
 	}
+
 	//character
-	foreach($_POST["characterName"] as $index => $value){
-		array_push($actorArray[$index],$value);
+	foreach ( $_POST[ "characterName" ] as $index => $value ) {
+		array_push( $actorArray[ $index ], $value );
 	}
+
 	//image
-	foreach($_POST["imageLink"] as $index => $value){
-		array_push($actorArray[$index],$value);
+	foreach ( $_POST[ "imageLink" ] as $index => $value ) {
+		array_push( $actorArray[ $index ], $value );
 	}
-	
-	$actor_Parse = $_POST["actorsInf"];
-	echo "actors: ";
-	echo $actor_Parse;
-	echo "<br>";
-	
 
-//	echo "File contents: ";
-//	echo "<br>";
-//	$str_json = file_get_contents( 'php://input' );
-//	print_r( $str_json );
-//	echo "<br>";
-
-	//testing
-	//	print_r($_POST["actorNameTest"]);
-
-	//	
-	//	
-	//	//info
-	//	foreach($_POST["actorDiv"] as $divIndex => $listOfValues){
-	//		print_r($divIndex);
-	//		print_r($listOfValues);
-	//		array_push($actorArray[$divIndex],array());
-	//		foreach($listOfValues["actorInfoDiv"] as $index => $value){
-	//			print_r($value);
-	//			array_push($actorArray[$divIndex][$index],$value);
-	//		}
-	//	}
-	//	
-	//	print_r($actorArray);
+	$actorsInfo = json_decode( $_POST[ "actorsInf" ] );
+	foreach ( $actorsInfo as $infoIndex => $infoSet ) {
+		$info = array();
+		foreach ( $infoSet as $index => $value ) {
+			array_push( $info, $value );
+		}
+		array_push( $actorArray[ $infoIndex ], $info );
+	}
 
 	//Key Information
 	$part_of_a_series = $_POST[ "part_of_a_series" ];
@@ -115,6 +100,17 @@
 	$key_storylines->$realLifeString = $realSLArray;
 
 	//actors
+	//	$actors->$actorArray;
+	$ac = array();
+	foreach ( $actorArray as $actorIndex => $actorValues ) {
+		$actorDeats = array(
+			"name" => $actorValues[ 0 ],
+			"character" => $actorValues[ 1 ],
+			"image" => $actorValues[ 2 ],
+			"info" => $actorValues[ 3 ]
+		);
+		array_push( $ac, $actorDeats );
+	}
 
 	//key information
 	$part_of_series_string = "part_of_a_series?";
@@ -131,49 +127,41 @@
 	//compile	
 	$jObj->important_information = $important_information;
 	$jObj->key_storylines = $key_storylines;
+	$jObj->actors = $ac;
 	$jObj->key_information = $key_information;
 	$jObj->other = $other;
-	$jsonMovie = array( $title => $jObj );
+	$jsonTitle = $title . $release;
+	$jsonMovie = array( $jsonTitle => $jObj );
 	$jsonFormat = json_encode( $jsonMovie );
 	//JSON_FORCE_OBJECT
 
 	echo "json format: ";
 	echo $jsonFormat;
 
-	//	$jsonFile = '
-	//	{
-	//		'.$title.' : {
-	//			"important_information" : {
-	//			
-	//				"title" : "'.$title.'",
-	//				
-	//			},
-	//			
-	//			"actors" : {
-	//			
-	//			},
-	//			
-	//			"other" : {
-	//			
-	//			}
-	//		}
-	//	}
-	//	';
+	//convert actors to html	
+	$htmlStr = "";
+	foreach ( $actorArray as $index => $value ) {
+		$htmlStr .= "<b>Name: </b>";
+		$htmlStr .= $value[ 0 ];
+		$htmlStr .= "<br>";
 
-	//	var testMov = {
-	//		"test" : {
-	//			"title" : "movie",
-	//			"genre" : "test"
-	//		}
-	//	
-	//};
+		$htmlStr .= "<b>Character: </b>";
+		$htmlStr .= $value[ 1 ];
+		$htmlStr .= "<br>";
 
+		$htmlStr .= "<b>Image: </b>";
+		$htmlStr .= $value[ 2 ];
+		$htmlStr .= "<br>";
+
+		$htmlStr .= "<li>";
+		$htmlStr .= implode( "<br><li>", $value[ 3 ] );
+
+		$htmlStr .= "<br>";
+		$htmlStr .= "<br>";
+
+	}
 
 	//EMAIL
-
-	$to = 'liamgooch@live.com.au';
-	$cc = 'ljohnson3013@gmail.com';
-	$subject = "CINEMATE - {$title}";
 	$message = "
 <html>
 <head>
@@ -213,12 +201,14 @@
 <h3>Key Movie Storylines</h3>
 	
 <div id='movieStoryLineDiv'>
+<li>
 {$movieStoryLines}
 </div>
 	
 <h3>Key Real-Life Storylines</h3>
 
 	<div id='realStoryLineDiv'>
+	<li>
 	{$realStoryLines}
 	</div>
 	
@@ -238,7 +228,10 @@
 
 <h2>Actors</h2>
 	
-	<div id='actorDiv'></div>
+	<div id='actorDiv'>
+		{$htmlStr}
+		<br>
+	</div>
 	
 
 <hr>
@@ -253,29 +246,48 @@
 ";
 	echo $message;
 
+	function sendEmail( $client, $title, $message ) {
+		//------- SERVER SIDED MAILING CODE ONLY ---------
+		$to = "";
+		if ( $client ) {
+			$to = 'ljohnson3013@gmail.com';
+		} else {
+			$to = 'liamgooch@live.com.au';
+		}
+
+		$subject = "CINEMATE - {$title}";
+
+		$headers[] = "MIME-Version: 1.0";
+		$headers[] = "Content-Type: text/html; charset=UTF-8";
+
+		//	$headers[] = "Cc: ljohnson3013@gmail.com";
+		$headers[] = "From: db_entry@cinemate.com";
+		$headers[] = "Reply-To: ljohnson3013@gmail.com";
+
+		$success = mail( $to, $subject, $message, implode( "\r\n", $headers ) );
+		if ( !$success ) {
+			print_r( error_get_last()[ 'message' ] );
+		}
+
+//		mail( $to, $subject, $message, implode( "\r\n", $headers ) );
+	}
+
+//	sendEmail( True, $title, $message );
+
 	$message .= "
 	
-		<input type='button' value='APPROVE'>
-		<input type='button' value='EDIT'>
+		<a href='goochystesting.tk/submitJSON.php?approved=1&jsonCode={$jsonFormat}' target='_blank'>Approve</a>
+		
+		<br>
+		
+		<a href='goochystesting.tk/submitJSON.php?approved=0&jsonCode={$jsonFormat}' target='_blank'>Edit</a>
+		
 		</body>
 		</html>
 	";
-	//$headers = 'From: db_entry@cinemate.com' . "\r\n" .
-	//    'Reply-To: ljohnson3013@gmail.com' . "\r\n";
 
-	$headers[] = "MIME-Version: 1.0";
-	$headers[] = "Content-Type: text/html; charset=UTF-8";
+	sendEmail( False, $title, $message );
 
-	$headers[] = "Cc: ljohnson3013@gmail.com";
-	$headers[] = "From: db_entry@cinemate.com";
-	$headers[] = "Reply-To: ljohnson3013@gmail.com";
-
-	//	$success = mail($to, $subject, $message, implode("\r\n", $headers));
-	//if (!$success) {
-	//   print_r(error_get_last()['message']);
-	//}
-
-	//mail($to, $subject, $message, $headers);
 	?>
 
 	<input type="button" value="Go Back" onClick="goBack()"/>
